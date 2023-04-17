dd5cb3f60eca80ad03ee97a08d4be0fd4909798b
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-17 07:29:32 +0200

    how to use

---
+++ src/components/LeftBar/Guide.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Guide() {
+  return (
+    <div className='w-[350px] text-white mt-6'>
+        <h2 className='font-bold text-sm text-orange-500 tracking-wider cursor-default'>HOW TO USE</h2>
+        <p className='text-sm mt-3 tracking-wide leading-[1.5] cursor-default'>You are not experienced in contributing to repositories ? Then just <span className='italic text-orange-500'>choose technology</span> in which you work and start searching by clicking Find button below in <span className='italic text-orange-500'>Beginner Friendly</span> section. If you already have some experience or looking for specify issue, then <span className='italic text-orange-500'>choose technology</span> and type your issue in <span className='italic text-orange-500'>Search Bar</span>.</p>
+    </div>
+  )
+}
