e6a5016305bf699c46368cb18f0a1b28b145a1f9
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-17 07:30:11 +0200

    reminder about choosing technology

---
+++ src/components/LeftBar/Reminder.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Reminder() {
+  return (
+    <div className='w-[350px] text-white mt-6'>
+        <h2 className='text-sm text-orange-500 font-bold tracking-wider cursor-default'>BEFORE YOU START SEARCHING</h2>
+        <p className='text-sm mt-3 tracking-wide leading-[1.5] cursor-default'>Make sure that you have choosen technology, at this moment page is not supporting searching without specyfying issue technology. Without it, content will not be loaded.</p>
+    </div>
+  )
+}
