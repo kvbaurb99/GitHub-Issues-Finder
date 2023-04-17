ea4f7df32c13f4c816c6ce44052458a6a22e0564
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-04-17 07:31:40 +0200

    noreferrer security

--- src/components/RightBar/Issue.jsx
+++ src/components/RightBar/Issue.jsx
@@ -16,7 +16,7 @@ export default function Issue({title, state, date, comments, link, description})
 
   return (
     <div onMouseLeave={() => setIsHover(false)} onMouseOver={() => setIsHover(true)} className={`text-white border border-gray-600 mt-4 p-4 cursor-pointer ${isHover ? 'bg-gray-600/30' : null} h-full rounded relative`}>
-        <a href={link} target='_blank'><p className={`font-bold cursor-pointer text-xl tracking-wider max-w-[90%] ${isHover ? 'text-orange-500' : null}`}>{title}</p></a>
+        <a href={link} rel='noreferrer' target='_blank'><p className={`font-bold cursor-pointer text-xl tracking-wider max-w-[90%] ${isHover ? 'text-orange-500' : null}`}>{title}</p></a>
         {description !== null ? <p className='text-sm tracking-wide mt-2'>{description.slice(0, 150)} {description.length > 150 ? '...' : null}</p> : null}
         <p className='absolute top-4 right-4 text-green-500 text-sm font-bold tracking-wide'>{state.charAt(0).toUpperCase() + state.slice(1)}</p>
         <div className='flex items-center gap-2 text-sm text-gray-400 mt-4'>
